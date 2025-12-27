6f0309580d1c19db591ad32ac1017d45a1b7dc16
zenin5479 <zenin5479@gmail.com>
2025-12-27 23:53:22 +0300

    Update 1.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace SyncAppClient
    {
       static void Main()
       {
-         Console.WriteLine("Тестирование HTTP сервера...\n");
+         Console.WriteLine("Тестирование HTTP сервера...");
          string baseUrl = "http://localhost:8080/";
          try
          {
