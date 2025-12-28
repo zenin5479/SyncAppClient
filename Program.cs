b84535d52f2c98ace9d13e8678061157b825f713
zenin5479 <zenin5479@gmail.com>
2025-12-28 10:58:46 +0300

    Update 3.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -156,7 +156,8 @@ namespace SyncAppClient
             catch (WebException ex)
             {
                Console.WriteLine("Ошибка: {0}", ex.Message);
-               if (ex.Response is HttpWebResponse httpResponse)
+               HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+               if (httpResponse != null)
                {
                   Console.WriteLine("Код статуса: {0}", httpResponse.StatusCode);
                   Console.WriteLine("Описание статуса: {0}", httpResponse.StatusDescription);
