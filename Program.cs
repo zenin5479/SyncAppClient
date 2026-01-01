caf8e63f629d18a935367d76104d6b1ce8b4effa
zenin5479 <zenin5479@gmail.com>
2026-01-01 22:54:38 +0300

    Update 5.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace SyncAppClient
       static void Main()
       {
          Console.WriteLine("Тестирование HTTP сервера...");
-         string baseUrl = "http://127.0.0.1:8888/connection/";
-         //string baseUrl = "http://127.0.0.1:8080/";
+         //string baseUrl = "http://127.0.0.1:8888/connection/";
+         string baseUrl = "http://127.0.0.1:8080/";
          try
          {
             // Тестирование GET запроса
